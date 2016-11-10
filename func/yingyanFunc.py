73749cc53022e22890763a964b0c81cb0ce372fd
watermelonharry <bongbongca@foxmail.com>
2016-11-10 14:12:44 +0800

    将障碍信息保存到本地ErrorInfo文件夹中。

--- func/yingyanFunc.py
+++ func/yingyanFunc.py
@@ -38,6 +38,16 @@ class YingyanFunc(QDialog, Ui_yingyan_web):
         self.toYingyanFuncSignal.connect(self.ExtractCommandData)
         #发送至pickFunc
         self.toPickPointSignal= toPickSignal
+        #故障信息存储文件
+        import os
+        PATH = '/'.join(os.getcwd().split('\\'))
+        self.errorFile = PATH + '/ErrorInfo/BugInfo-'+'-'.join(time.ctime().split(' ')).replace(':','-',10) +'.dat'
+        try:
+            with open(self.errorFile, 'a') as errorFile:
+                errorFile.write('<Error File created at ' + time.ctime()+'>\n')
+                errorFile.write('<Error File created at ' + time.ctime() + '>')
+        except Exception as e:
+            print ('error in creating errorFile:' ,e.message, 'please reopen the program')
 
         #upload data to BAIDU
         self.uploader = GpsUploader(updateMainSignal=updateMainSignal,toPickPointSignal= toPickSignal)
@@ -96,19 +106,27 @@ class YingyanFunc(QDialog, Ui_yingyan_web):
             else:
                 if data[1] == 'P':      #command 2
                     #todo: param check
-                    pass
+                    return
                 else:
                     argList = None
 
                 if data[1] == 'S':      #command 3
                     #todo: param set
-                    pass
+                    return
                 else:
                     argList = None
 
                 if data[1][0] == 'D':      #command 4
                     #todo: set points
                     self.SendToPickFunc(strArg)
+                    return
+                else:
+                    argList = None
+
+                if data[1] == 'E':      #命令4，飞行器发送，故障信息上传
+                    #todo: 故障信息转存到文件
+                    self.SaveErrorToFile(data[2:6])
+                    return
                 else:
                     argList = None
         else:
@@ -121,5 +139,19 @@ class YingyanFunc(QDialog, Ui_yingyan_web):
         """
         #test
         self.updateMainSignal.emit('yinyan-to pickpiont:' + str(strArg)[:-1])
-
         self.toPickPointSignal.emit(strArg)
+
+    def SaveErrorToFile(self,errorList):
+        """
+        保存故障信息到本地
+        输入参数列表：[故障序号，故障类型，经度，纬度]
+        本地提供：本地时间
+        :return:
+        """
+        writeList = errorList + [time.ctime()]
+        try:
+            with open(self.errorFile,'a') as errorFile:
+                errorFile.write(','.join(writeList) + '\n')
+        except Exception as e:
+            print('error in yingyan-SaveErrorToFile:', e.message)
+
